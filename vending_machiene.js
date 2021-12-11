87217bf018dfe3f80f03308fb65888f012fa0573
MeriBarseghyan2003 <92750263+MeriBarseghyan2003@users.noreply.github.com>
2021-12-11 15:45:22 +0400

    Add files via upload

---
+++ vending_machiene.js
@@ -0,0 +1,23 @@
+const products = [
+
+{
+    name: "chips",
+    price: "2.50"
+},
+{
+    name:"kit kat",
+    price: "2.00"
+},
+{
+    name: "twix",
+    price: "2.00"
+},
+{
+    name: "apple",
+    price: "0.50"
+},
+{
+    name :"water",
+    price: "1.00"
+}
+] ;
